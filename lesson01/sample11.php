151eb55fa0a0518a71db3ad5a40437b244e5855d
naaaaaaamiii <1867031@g.iuhw.ac.jp>
2024-03-23 15:47:43 +0900

    sample11

---
+++ lesson01/sample11.php
@@ -0,0 +1,13 @@
+<?php
+$fruits = ['apple'=> 'りんご',
+           'grape'=> 'ぶどう',
+           'lemon'=> 'レモン',
+           'tomato'=>'トマト'
+];
+?>
+<dl>
+    <?php foreach ($fruits as $english => $japanese): ?>
+        <dt><?php echo $english; ?></dt>
+        <dd><?php echo $japanese; ?></dd>
+    <?php endforeach; ?>
+</dl>
